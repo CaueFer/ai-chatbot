0e609400dc1fa5c8de38f2cf0f1e8c6d47472262
CaueFer <cauefernandescaetano@gmail.com>
2024-08-29 11:44:02 -0300

    add message components and tailwind merge

--- src/components/chatWrapper.tsx
+++ src/components/chatWrapper.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useChat } from "ai/react";
+import { Messages } from "./Messages";
 
 export const ChatWrapper = ({ sessionId }: { sessionId: string }) => {
   const { messages, handleInputChange, handleSubmit, input } = useChat({
@@ -11,12 +12,12 @@ export const ChatWrapper = ({ sessionId }: { sessionId: string }) => {
   return (
     <div className="relative min-h-full bg-zinc-900 flex divide-y divide-zinc-700 flex-col justify-between gap-2">
       <div className="flex-1 text-white bg-zinc-800 justify-between flex flex-col">
-        {JSON.stringify(messages)}
+        <Messages messages={messages} />;
       </div>
 
       <form onSubmit={handleSubmit}>
         <input className="text-black" value={input} onChange={handleInputChange} type="text" />
-        <button type="submit">enviar</button>
+        <button type="submit">send</button>
       </form>
     </div>
   );
