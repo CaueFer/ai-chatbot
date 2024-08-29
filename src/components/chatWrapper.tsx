e65c173a8248c92b9e8a9b7188efef6ccb211498
CaueFer <cauefernandescaetano@gmail.com>
2024-08-29 12:06:05 -0300

    add nextui lib and finish messages component

--- src/components/chatWrapper.tsx
+++ src/components/chatWrapper.tsx
@@ -12,7 +12,7 @@ export const ChatWrapper = ({ sessionId }: { sessionId: string }) => {
   return (
     <div className="relative min-h-full bg-zinc-900 flex divide-y divide-zinc-700 flex-col justify-between gap-2">
       <div className="flex-1 text-white bg-zinc-800 justify-between flex flex-col">
-        <Messages messages={messages} />;
+        <Messages messages={messages} />
       </div>
 
       <form onSubmit={handleSubmit}>
